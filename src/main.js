0dba83ad3aa53ca3377e82b1930913a156cc6c00
Tao <taozamorano@gmail.com>
2025-07-22 05:33:57 +0100

    update

--- src/main.js
+++ src/main.js
@@ -1,1 +1,21 @@
 console.log("Oli!");
+
+document.addEventListener("DOMContentLoaded", function () {
+  const menuButton = document.getElementById("mobile-menu-button");
+  const mobileMenu = document.getElementById("mobile-menu");
+
+  menuButton.addEventListener("click", function () {
+    if (mobileMenu.classList.contains("max-h-0")) {
+      mobileMenu.classList.remove("max-h-0");
+      mobileMenu.classList.add("max-h-60"); // adjust as needed
+    } else {
+      mobileMenu.classList.remove("max-h-60");
+      mobileMenu.classList.add("max-h-0");
+    }
+  });
+
+  const yearSpan = document.getElementById("current-year");
+  if (yearSpan) {
+    yearSpan.textContent = new Date().getFullYear();
+  }
+});
