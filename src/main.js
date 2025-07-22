41c7b11e089da7501fc1dc05975139abd34ec0aa
Tao <taozamorano@gmail.com>
2025-07-22 13:37:43 +0100

    fix expand mobile nav

--- src/main.js
+++ src/main.js
@@ -1,17 +1,21 @@
 console.log("Oli!");
 
 document.addEventListener("DOMContentLoaded", function () {
-  const menuButton = document.getElementById("mobile-menu-button");
-  const mobileMenu = document.getElementById("mobile-menu");
+  const burger = document.querySelector(".burger");
+  const links = document.querySelector(".links");
+  const bar1 = document.querySelector(".bar1");
+  const bar2 = document.querySelector(".bar2");
+  const bar3 = document.querySelector(".bar3");
 
-  menuButton.addEventListener("click", function () {
-    if (mobileMenu.classList.contains("max-h-0")) {
-      mobileMenu.classList.remove("max-h-0");
-      mobileMenu.classList.add("max-h-60"); // adjust as needed
-    } else {
-      mobileMenu.classList.remove("max-h-60");
-      mobileMenu.classList.add("max-h-0");
-    }
+  burger.addEventListener("click", () => {
+    links.classList.toggle("expanded");
+    bar1.classList.toggle("transform");
+    bar1.classList.toggle("translate-y-1");
+    bar1.classList.toggle("-rotate-45");
+    bar2.classList.toggle("hidden");
+    bar3.classList.toggle("transform");
+    bar3.classList.toggle("-translate-y-1");
+    bar3.classList.toggle("rotate-45");
   });
 
   const yearSpan = document.getElementById("current-year");
