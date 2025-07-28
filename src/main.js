0839f155cc8887282067dbf43264d55e73c3806c
Tao <taozamorano@gmail.com>
2025-07-29 00:26:27 +0100

    modularising css and assets

--- src/main.js
+++ src/main.js
@@ -2,39 +2,27 @@ console.log("Lorem Ipsum");
 
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
-    console.log(entry.intersectionRatio);
-
-    if (
-      entry.isIntersecting
-      // && entry.intersectionRatio > 0.2
-    ) {
+    if (entry.isIntersecting) {
       entry.target.classList.add("reveal-active");
     }
   });
 });
 
 document.addEventListener("DOMContentLoaded", function () {
+  // Nav
   const burger = document.querySelector(".burger");
   const links = document.querySelector(".links");
-  const bar1 = document.querySelector(".bar1");
-  const bar2 = document.querySelector(".bar2");
-  const bar3 = document.querySelector(".bar3");
 
   burger.addEventListener("click", () => {
-    links.classList.toggle("expanded");
-    bar1.classList.toggle("transform");
-    bar1.classList.toggle("translate-y-1");
-    bar1.classList.toggle("-rotate-45");
-    bar2.classList.toggle("hidden");
-    bar3.classList.toggle("transform");
-    bar3.classList.toggle("-translate-y-1");
-    bar3.classList.toggle("rotate-45");
+    links.classList.toggle("open");
+    burger.classList.toggle("open");
   });
 
+  // Reveal
   document.querySelectorAll(".reveal").forEach((el) => observer.observe(el));
 
-  const yearSpan = document.getElementById("current-year");
-  if (yearSpan) {
-    yearSpan.textContent = new Date().getFullYear();
-  }
+  // Copyleft
+  const copyleftYear = document.getElementById("current-year");
+  if (!copyleftYear) console.log("no copyleft year container found");
+  copyleftYear.textContent = new Date().getFullYear();
 });
